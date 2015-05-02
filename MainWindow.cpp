2cbc713284a48ae0b766bd04d9beb41f66d94f80
Lukas Lihotzki <lukas@lihotzki.de>
2015-05-02 16:54:16 +0200

    Create GraphicsScene

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -7,9 +7,24 @@ MainWindow::MainWindow(QWidget *parent) :
 	setupUi(this);
 }
 
+void MainWindow::resetGraphicsView()
+{
+	QGraphicsScene* oldScene = graphicsView->scene();
+	if (oldScene) {
+		graphicsView->setScene(nullptr);
+		delete oldScene;
+	}
+}
+
 void MainWindow::on_btnGenerate_clicked()
 {
 	std::string input = plainTextEdit->toPlainText().toStdString();
 	const char* begin = &*input.cbegin();
 	const char* end = &*input.cend();
+
+	resetGraphicsView();
+	QGraphicsScene* scene = new QGraphicsScene(this);
+	scene->setSceneRect(0.f, 0.f, 200.f, 200.f);
+	scene->addLine(0.f, 0.f, 100.f, 100.f);
+	graphicsView->setScene(scene);
 }
